0c6c6cb8eea5f83fbbbf82a9cc65969874b454c8
coraleaf0602 <zhangco@tcd.ie>
2024-02-18 15:09:51 +0000

    made function for calling model, training model and also made a page that displayed a random dell page

--- Backend/test.py
+++ Backend/test.py
@@ -1,9 +1,48 @@
 from flask import Flask, redirect, url_for, render_template, request, session, jsonify 
 import requests 
+import json 
 
 app = Flask(__name__)
 
-test_data = """Instructions
+# @app.route("/")
+# def hello_world():
+#     return f"<p>{test_data}</p>"
+
+# The endpoint for authenticating your API request.
+endpoint = "https://dellsemanticproductlink.cognitiveservices.azure.com/"
+project_name = "SemanticProductLink" 
+api_version = "2022-10-01-preview"
+
+# unique identifier assigned to the text analysis job
+job_id = ""
+    
+    
+headers = { 
+    # This specifies that the request body is in JSON format
+    "Content-Type": "application/json",  
+    # Remember to change this to an environment variable saved on Mac 
+    # export API_KEY=value in terminal 
+    "Ocp-Apim-Subscription-Key": ""  
+}
+
+@app.route("/")
+def home(): 
+    return render_template("basicWebsite.html")
+
+# Random page on dell to display and integrate - buggy 
+@app.route("/dell")
+def dell(): 
+    return render_template("dellRandomPage.html")
+
+# Method to call the Azure model 
+@app.route("/predict", methods=["POST", "GET"])
+def predict():
+    
+    # This data is not the same as the JSON file used to train the model, 
+    # as it does not contain labeled annotations but rather the input text 
+    # that the model will process.
+    # For example, text data from the knowledge base articles that we're using 
+    test_data = """Instructions
 Need help with setting up a Dell docking station with a laptop? This article provides information about how to connect and set up a Dell docking station with a laptop. You can also find common troubleshooting steps and answers to some frequently asked questions.
 
 Docking stations help provide the modern worker with the best of both worlds, the benefits of a desktop computer without sacrificing the portability of a laptop. You may connect external monitors, Ethernet for reliable connection, USB ports for connecting devices, and a full-sized keyboard and mouse, among other things.
@@ -19,27 +58,56 @@ Thunderbolt 3 - Thunderbolt 3 docking stations link all devices to the laptop us
 DisplayPort over USB-C - USB-C docking station links all devices to the laptop using a single USB-C cable. Extend the traditional USB capabilities with native multi-display video, audio, data, and power delivery (on select Dell laptops) to charge the laptop with a single USB-C cable. The reversible connector is convenient to use USB-C, with no wrong orientation.
 USB 3.0 - Universal docking stations work with DisplayLink technology that enables docking features over USB. The universal docking solution enables multiple monitors, audio, Ethernet, and other USB devices to be connected to laptops through USB. Works best with USB 3.0 ports on the laptop.
 WiGig (wireless) - WiGig or wireless docking stations work with a laptop that is configured with a WiGig adapter. Only select Dell laptops support WiGig technology."""
+    # construct post request 
+    post_url = f"{endpoint}/language/authoring/analyze-text/projects/{project_name}/:import?api-version={api_version}"
+    
+    # GET request to get the status of your importing your project
+    get_url = f"{endpoint}/language/authoring/analyze-text/projects/{project_name}/import/jobs/{job_id}?api-version={api_version}"
+    
+    # Request body 
+    requests_body = "/Users/zhangc/Documents/CS Year 2/Semester 2/CSU22013 Software Engineering Project I /SwEng-Group-14-SemanticProductLink/Training_Data/weijian.json"
+   
+    # Make POST request to Azure model endpoint 
+    
+    
+    # Possible error scenarios for this request:
+    # The selected resource doesn't have proper permissions for the storage account.
+    # The storageInputContainerName specified doesn't exist.
+    # Invalid language code is used, or if the language code type isn't string.
+    # multilingual value is a string and not a boolean.
+    if request.method == "POST": 
+        response = requests.post(url=post_url, headers=headers, json=requests_body)
+        if response.status_code == 202:
+            return f"<p>Success</p>"
+        else:
+            print(response.status_code)
+            return f"<p>Fail</p>"
+    
+    else: 
+        response = requests.get(url=get_url, headers=headers)
+        return(response.get_json()) 
 
-# @app.route("/")
-# def hello_world():
-#     return f"<p>{test_data}</p>"
 
-@app.route("/")
-def predict():
- 
-    data = "/Users/zhangc/Documents/CS Year 2/Semester 2/CSU22013 Software Engineering Project I /SwEng-Group-14-SemanticProductLink/Training_Data/weijian.json"
-    # Make POST request to Azure model endpoint
-    url = "https://dellsemanticproductlink.cognitiveservices.azure.com/language/authoring/analyze-text/projects/hello/:import?api-version=2022-10-01-preview"
-    headers = {
-        "Ocp-Apim-Subscription-Key": "b3bb51730d9c4d6f92abd3d817d46043"
-    }
-    response = requests.post(url=url, headers=headers, json=data)
+@app.route("/train", methods=["POST", "GET"])
+def train(): 
+    # After your project has been imported, you can start training your model.
+    # Submit a POST request using the following URL, headers, and JSON body to submit a training job.
+    post_url = f"{endpoint}/language/authoring/analyze-text/projects/{project_name}/:train?api-version={api_version}"
+    request_body = {
+        "modelLabel": "{MODEL-NAME}",
+        "trainingConfigVersion": "{CONFIG-VERSION}",
+        "evaluationOptions": {
+            "kind": "percentage",
+            "trainingSplitPercentage": 80,
+            "testingSplitPercentage": 20
+        }
+    }   
+    body_json = json.dumps(request_body, indent=4)
 
-    if response.status_code == 202:
-        return f"<p>Labels imported succesfully</p>"
-    else:
-        print(response.status_code)
-        return f"<p>Fail</p>"
+    if request.method == "POST": 
+        response = requests.post(url=post_url, headers=headers, json=body_json)
+        return (response.status_code)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)

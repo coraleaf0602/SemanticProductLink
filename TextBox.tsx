16c01690cde40a33b3f66984df8fc75c64372b3e
Cillian23 <127849681+Cillian23@users.noreply.github.com>
2024-02-24 14:04:26 +0000

    TextBox.tsx

--- TextBox.tsx
+++ TextBox.tsx
@@ -38,28 +38,3 @@ const TextBox: React.FC<Props> = ({ displayText }) => {
 
 export default TextBox;
 
-/*interface props {
-  children: string;
-  onClick: () => void;
-   displayText:  () => void;
-}
-const TextBox = ({ children, onClick, displayText }: props) => {
-  return (
-    <div className="mb-3">
-      <label htmlFor={children} className="form-label">
-        Enter Text
-      </label>
-      <input
-        type="email"
-        className="form-control"
-        id="exampleFormControlInput1"
-        placeholder="E.G. Docking Station"
-      ></input>
-      <button className={"btn btn-primary"} onClick={displayText}>
-        Submit
-      </button>
-    </div>
-  );
-};
-
-export default TextBox; */

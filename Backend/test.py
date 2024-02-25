3231522636fb2dd4bfc90ec713569c0d0cc865fe
coraleaf0602 <zhangco@tcd.ie>
2024-02-25 11:12:50 +0000

    made a simple fail and success html page and while loop that continously calls get requests until the task has been successfully completed

--- Backend/test.py
+++ Backend/test.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, url_for, render_template, request, session, j
 import requests 
 import json 
 import os 
+import time 
 
 app = Flask(__name__)
 
@@ -64,10 +65,9 @@ WiGig (wireless) - WiGig or wireless docking stations work with a laptop that is
     # post_url = f"{endpoint}/language/authoring/analyze-text/projects/{project_name}/:import?api-version={api_version}"
     post_url = "https://dellsemanticproductlink.cognitiveservices.azure.com/language/analyze-text/jobs?api-version=2022-10-01-preview"
     # GET request to get the status of your importing your project
-    get_url = "https://dellsemanticproductlink.cognitiveservices.azure.com/language/analyze-text/jobs?api-version=2022-10-01-preview"
-    # Request body 
-    with open("/Users/zhangc/Documents/CS Year 2/Semester 2/CSU22013 Software Engineering Project I /SwEng-Group-14-SemanticProductLink/Training_Data/weijian.json", "r") as f:
-        requests_body = json.load(f)
+   
+    # with open("/Users/zhangc/Documents/CS Year 2/Semester 2/CSU22013 Software Engineering Project I /SwEng-Group-14-SemanticProductLink/Training_Data/weijian.json", "r") as f:
+    #     requests_body = json.load(f)
     requests_body = {
         "displayName":"Extracting entities",
         "analysisInput":{
@@ -100,6 +100,7 @@ WiGig (wireless) - WiGig or wireless docking stations work with a laptop that is
     # Invalid language code is used, or if the language code type isn't string.
     # multilingual value is a string and not a boolean.
     # if request.method == "POST": 
+    print(headers.values())
     response = requests.post(url=post_url, headers=headers, json=requests_body)
     # Check response status code
     print("Response Status Code:", response.status_code)
@@ -109,15 +110,41 @@ WiGig (wireless) - WiGig or wireless docking stations work with a laptop that is
 
     # Check response headers
     print("Response Headers:", response.headers)
-    if response.status_code == 202:
-        return f"<p>Success</p>"
-    else:
-        print(response.status_code)
-        return f"<p>Fail</p>"
     
-    # else: 
-    #     response = requests.get(url=get_url, headers=headers)
-    # return("get method") 
+    # In the response headers, extract operation-location. 
+    # You can use this URL to query the task completion status and get the results when task is completed.
+    get_url = response.headers.get('operation-location')
+    print("Operation Location:", get_url)
+
+    # You will receive a 202 response indicating that your task has been submitted successfully.
+    if response.status_code == 202:
+        # duration = 10 # Set the amount of time we want to continously run the get requests for 
+        # start_time = time.time() # Get the start time
+        task_running = True
+        # Run the while loop until the specified duration is reached
+        while task_running:
+            response = requests.get(url=get_url, headers=headers)
+            # Check response status code
+            # print("Response Status Code:", response.status_code)
+
+            # # Check response content
+            # print("Response Content:", response.text)
+            response_body = response.json()
+            
+            response_status = response_body.get('status')
+            print(response_status)
+            # # Check response headers
+            # print("Response Headers:", response.headers)
+
+            # # Add a small delay to avoid consuming too much CPU
+            # time.sleep(1)  # Sleep for 1 second
+            if response_status == "succeeded":
+                task_running = False
+        return render_template("success.html")
+    else:
+        return render_template("fail.html")
+    
+
 
 # Querying our model 
 # Reference link: https://learn.microsoft.com/en-gb/azure/ai-services/language-service/custom-named-entity-recognition/how-to/call-api?tabs=rest-api#send-an-entity-recognition-request-to-your-model

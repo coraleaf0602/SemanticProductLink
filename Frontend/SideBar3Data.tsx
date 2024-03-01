acfa61732a2a52f7107de9ec03ac63006f726735
Daniel Preda <predad@tcd.ie>
2024-03-01 15:21:25 +0000

    move react components to frontend

---
+++ Frontend/SideBar3Data.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SideBar3Data = [
+  {
+    image1: "./xps2Standard.jpg",
+    image2: ".xps2Dynamic.jpg",
+    title: "Image1",
+    path: "https://www.dell.com/en-ie",
+    price: "$300",
+    className: "nav-text",
+  },
+  {
+    image1: "./xps2Dynamic.jpg",
+    image2: "./xps2Standard.jpg",
+    title: "Image2",
+    path: "https://www.dell.com/en-ie",
+    price: "$300",
+    className: "nav-text",
+  },
+  {
+    image1: "./alienware.jpg",
+    title: "Image3",
+    path: "https://www.dell.com/en-ie",
+    price: "$300",
+    className: "nav-text",
+  },
+  {
+    image1: "./alienwareD.jpg",
+    title: "Image4",
+    path: "https://www.dell.com/en-ie",
+    price: "$300",
+    className: "nav-text",
+  },
+  {
+    image1: "./xps13.png",
+    title: "Image5",
+    path: "https://www.dell.com/en-ie",
+    price: "$300",
+    className: "nav-text",
+  },
+];

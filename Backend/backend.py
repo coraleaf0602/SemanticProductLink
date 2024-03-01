059aacd3caf6e6f2b03130095d07faa5b8353d09
Daniel Preda <predad@tcd.ie>
2024-03-01 18:01:23 +0000

    added robin's list of category-link pairs to lookup

--- Backend/backend.py
+++ Backend/backend.py
@@ -4,15 +4,28 @@ import requests
 import json 
 import os 
 
-link_lookup_table = {"XPS":"https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/xps-laptops",
-                     "Inspiron":"https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/inspiron-laptops",
-                     "Laptops and 2-in-1 PC": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops",
-                     "Monitors": "https://www.dell.com/en-ie/shop/everyday/sac/monitors/everyday-monitors",
-                     "Keyboards and Mice": "https://www.dell.com/en-ie/shop/keyboards/ar/8545/keyboard-mice-combos?appliedRefinements=43910",
-                     "Docking Stations": " https://www.dell.com/en-ie/shop/docks-and-stands/ar/8408",
-                     "Webcams and Video Conferencing":"https://www.dell.com/en-ie/shop/web-cameras/ar/8332",
-                     "Desktops":"https://www.dell.com/en-ie/shop/desktop-computers/sc/desktops"
-                     }
+link_lookup_table = {"Laptops and 2-in-1 PC": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sc/laptops", 
+                    "XPS": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/xps-laptops", 
+                    "Latitude": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/latitude-laptops", 
+                    "Inspiron": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/inspiron-laptops", 
+                    "Vostro": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/vostro-laptops", 
+                    "Alienware": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/alienware-laptops",
+                    "Precision": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/precision-laptops",
+                    "Desktops": "https://www.dell.com/en-ie/shop/desktop-computers/sc/desktops",
+                    "OptiPlex": "https://www.dell.com/en-ie/shop/desktop-computers/sr/desktops/optiplex-desktops", 
+                    "Monitors": "https://www.dell.com/en-ie/shop/computer-monitors/ar/8605",
+                    "Docking Stations": "https://www.dell.com/en-ie/shop/docks-and-stands/ar/5441/docking-stations?appliedRefinements=962", 
+                    "Audio": "https://www.dell.com/en-ie/shop/audio/ac/8310", 
+                    "Keyboards and Mice":"https://www.dell.com/en-ie/shop/keyboards-mice/ar/6591",
+                    "Hard Drives, SSDs and Storage": "https://www.dell.com/en-ie/shop/hard-drives-storage/ar/8496", 
+                    "Webcams and Video Conferencing": "https://www.dell.com/en-ie/shop/web-cameras/ar/8332", 
+                    "Batteries, Chargers and Power Adapters": "https://www.dell.com/en-ie/shop/pc-accessories/ar/5436/batteries-chargers-power-adapters?appliedRefinements=44426", 
+                    "Cables and Adapters": "https://www.dell.com/en-ie/shop/cables/ar/8168/cables-adapters?appliedRefinements=44427",
+                    "Security and Protection": "https://www.dell.com/en-ie/shop/security-protection/ar/7664",
+                    "Parts, Batteries and Upgrades": "https://www.dell.com/en-ie/shop/parts-batteries-upgrades/ar/7566", 
+                    "Wi-Fi and Networking": "https://www.dell.com/en-ie/shop/wifi-and-networking/ar/4011",
+                    "Gaming Accessories": "https://www.dell.com/en-ie/shop/gaming-gaming-accessories/ac/6488"
+                    }
 
 app = Flask(__name__)
 

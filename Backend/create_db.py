048147b1a9057edd5b4637c9abb4add671913108
WeijianWWWW <123949187+WeijianWWWW@users.noreply.github.com>
2024-03-09 18:22:25 +0000

    Update create_db.py

--- Backend/create_db.py
+++ Backend/create_db.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('entities.db')
 cursor = conn.cursor()
 
-command1 = 'create table entity (id Integer Primary Key, Entity_Name text, ImagePath text)'
+command1 = 'create table entity (Entity_Name text Primary Key, URL text, ImagePath text)'
 cursor.execute(command1)
 
 conn.commit()

9ed2d63d171ec2c449bc34bf508a42d349c6b3b7
Daniel Preda <predad@tcd.ie>
2024-03-11 16:55:35 +0000

    Integrated database with backend

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,45 +1,14 @@
 from flask import Flask, redirect, url_for, render_template, request, session, Response
+from database import retrieve
 import flask_cors
 import requests 
 import json 
 import os 
-import sqlite3
-
-link_lookup_table = {"Laptops and 2-in-1 PC": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sc/laptops", 
-                    "XPS": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/xps-laptops", 
-                    "Latitude": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/latitude-laptops", 
-                    "Inspiron": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/inspiron-laptops", 
-                    "Vostro": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/vostro-laptops", 
-                    "Alienware": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/alienware-laptops",
-                    "Precision": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/precision-laptops",
-                    "Desktops": "https://www.dell.com/en-ie/shop/desktop-computers/sc/desktops",
-                    "OptiPlex": "https://www.dell.com/en-ie/shop/desktop-computers/sr/desktops/optiplex-desktops", 
-                    "Monitors": "https://www.dell.com/en-ie/shop/computer-monitors/ar/8605",
-                    "Docking Stations": "https://www.dell.com/en-ie/shop/docks-and-stands/ar/5441/docking-stations?appliedRefinements=962", 
-                    "Audio": "https://www.dell.com/en-ie/shop/audio/ac/8310", 
-                    "Keyboards and Mice":"https://www.dell.com/en-ie/shop/keyboards-mice/ar/6591",
-                    "Hard Drives, SSDs and Storage": "https://www.dell.com/en-ie/shop/hard-drives-storage/ar/8496", 
-                    "Webcams and Video Conferencing": "https://www.dell.com/en-ie/shop/web-cameras/ar/8332", 
-                    "Batteries, Chargers and Power Adapters": "https://www.dell.com/en-ie/shop/pc-accessories/ar/5436/batteries-chargers-power-adapters?appliedRefinements=44426", 
-                    "Cables and Adapters": "https://www.dell.com/en-ie/shop/cables/ar/8168/cables-adapters?appliedRefinements=44427",
-                    "Security and Protection": "https://www.dell.com/en-ie/shop/security-protection/ar/7664",
-                    "Parts, Batteries and Upgrades": "https://www.dell.com/en-ie/shop/parts-batteries-upgrades/ar/7566", 
-                    "Wi-Fi and Networking": "https://www.dell.com/en-ie/shop/wifi-and-networking/ar/4011",
-                    "Gaming Accessories": "https://www.dell.com/en-ie/shop/gaming-gaming-accessories/ac/6488"
-                    }
 
 app = Flask(__name__)
-#  after run create_db.py, the database is created 
-# conn = sqlite3.connect('eneities.db')
-# cursor = conn.cursor()
-# command = 'insert into entity (Entity_Name,URL,ImagePath) VALUES (Entity_Name,URLString,ImagePath_String)'
-# cursor.execute(command)
-# cursor.close()
-# conn.close()
 
 api_key = os.environ.get("API_KEY")
-
-
+    
 headers = { 
     "Content-Type": "application/json",   
     "Ocp-Apim-Subscription-Key": api_key  
@@ -88,8 +57,9 @@ def get_task():
         categories = set(categories)
         links = []
         for category in categories:
-            if category in link_lookup_table.keys():
-                links.append({"category":category,"link":link_lookup_table[category]})
+            row = retrieve(category)
+            if (row != None):
+                links.append({"category":row[0],"link":row[1]})
         return {"category_links":links}
     elif response.status_code == 200:
         return Response(status=202,response={"response-status":response_status})
--- Backend/database.py
+++ Backend/database.py
@@ -2,6 +2,29 @@ import sqlite3
 
 #Creates new database if one with the same name doesn't already exist
 def create_table():
+    link_lookup_table = {"Laptops and 2-in-1 PC": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sc/laptops", 
+                    "XPS": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/xps-laptops", 
+                    "Latitude": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/latitude-laptops", 
+                    "Inspiron": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/inspiron-laptops", 
+                    "Vostro": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/vostro-laptops", 
+                    "Alienware": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/alienware-laptops",
+                    "Precision": "https://www.dell.com/en-ie/shop/laptop-computers-2-in-1-pcs/sr/laptops/precision-laptops",
+                    "Desktops": "https://www.dell.com/en-ie/shop/desktop-computers/sc/desktops",
+                    "OptiPlex": "https://www.dell.com/en-ie/shop/desktop-computers/sr/desktops/optiplex-desktops", 
+                    "Monitors": "https://www.dell.com/en-ie/shop/computer-monitors/ar/8605",
+                    "Docking Stations": "https://www.dell.com/en-ie/shop/docks-and-stands/ar/5441/docking-stations?appliedRefinements=962", 
+                    "Audio": "https://www.dell.com/en-ie/shop/audio/ac/8310", 
+                    "Keyboards and Mice":"https://www.dell.com/en-ie/shop/keyboards-mice/ar/6591",
+                    "Hard Drives, SSDs and Storage": "https://www.dell.com/en-ie/shop/hard-drives-storage/ar/8496", 
+                    "Webcams and Video Conferencing": "https://www.dell.com/en-ie/shop/web-cameras/ar/8332", 
+                    "Batteries, Chargers and Power Adapters": "https://www.dell.com/en-ie/shop/pc-accessories/ar/5436/batteries-chargers-power-adapters?appliedRefinements=44426", 
+                    "Cables and Adapters": "https://www.dell.com/en-ie/shop/cables/ar/8168/cables-adapters?appliedRefinements=44427",
+                    "Security and Protection": "https://www.dell.com/en-ie/shop/security-protection/ar/7664",
+                    "Parts, Batteries and Upgrades": "https://www.dell.com/en-ie/shop/parts-batteries-upgrades/ar/7566", 
+                    "Wi-Fi and Networking": "https://www.dell.com/en-ie/shop/wifi-and-networking/ar/4011",
+                    "Gaming Accessories": "https://www.dell.com/en-ie/shop/gaming-gaming-accessories/ac/6488"
+                    }
+    
     conn = sqlite3.connect('test.db')
     cur = conn.cursor()
 
@@ -14,6 +37,8 @@ def create_table():
         )
     '''
     cur.execute(sql)
+    for key, item in link_lookup_table.items():
+        insert(key,item,"","")
     conn.commit()
     conn.close()
 
@@ -33,16 +58,14 @@ def retrieve(name):
     cur.execute('''SELECT * FROM TestTable WHERE name = ?''', (name,))
     row = cur.fetchone()
     conn.close()
-    return row
+    if row == None : return None
+    res = []
+    for element in row:
+        res.append(element)
+    return res
+
+if __name__ == "__main__":
+    create_table()
 
 
-create_table()
 
-insert("Laptop", "https://examplelaptop.com", "/path/to/image.jpg", "this is a laptop")
-insert("Monitor", "https://examplemonitor.com", "/path/to/image2.jpg", "this is a monitor")
-insert("Adapter", "https://exampleadapter.com", "/path/to/image3.jpg", "this is an adapter")
-
-
-TestTable = retrieve("Adapter")
-for entity in TestTable:
-    print(entity)

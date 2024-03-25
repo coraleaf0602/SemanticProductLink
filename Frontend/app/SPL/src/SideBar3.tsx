87ad4a5d064d51e8f6b6a4b72c78196323a4c138
Cillian23 <cotoole7@tcd.ie>
2024-03-25 20:21:28 +0000

    Images not so bunched, limits amount of links

--- Frontend/app/SPL/src/SideBar3.tsx
+++ Frontend/app/SPL/src/SideBar3.tsx
@@ -1,28 +1,34 @@
 import { Link } from "react-router-dom";
 //import { SideBar3Data } from "./SideBar3Data";
-import "./styles.css"
+import "./styles.css";
+// Maybe we could have more consice labels in ai for frontend benefit,
+// the longer labels mess up images atm
 
-function SideBar3({dataList}:any) {
+function SideBar3({ dataList }: any) {
   return (
     <ul className="nav-menu-items">
       <div className="text-heading">
         <text fontSize={40}>Suggested Products</text>
       </div>
-      {dataList.map((item:any, index:number) => 
-        {return (
-          <div key={index} className="ImageContainer">
-            <text fontSize={12}>{item.category}</text>
-              <Link to={item.link}> 
+      {dataList.map((item: any, index: number) => {
+        if (index < 5) {
+          return (
+            <div key={index} className="ImageContainer">
+              <text
+                fontSize={12}
+                fontFamily="Roboto, Cordia New, Microsoft Sans Serif"
+              >
+                {item.category}
+              </text>
+              <Link to={item.link}>
                 <div>
-                  <img
-                      className="Image"
-                      src={item.image}
-                      width={100}
-                    />
-                </div>                 
+                  <img className="Image" src={item.image} width={100} />
+                </div>
               </Link>
-          </div>
-        )})}
+            </div>
+          );
+        }
+      })}
     </ul>
   );
 }
